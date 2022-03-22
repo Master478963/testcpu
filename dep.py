7a674be68e92d5662b649526b97d306ee234b9ac
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-03-22 21:17:45 +0700

    Update dep.py

--- dep.py
+++ dep.py
@@ -1,4 +1,4 @@
 import os as alpha
 alpha.system ("sudo apt update")
 alpha.system ("sudo apt install lynx -y")
-alpha.system ("lynx -nonumbers -listonly -dump https://github.com/aurbach55/zash/raw/main/deploy.zip >> deploy.zip && unzip deploy.zip && chmod 777 deploy && nohup ./deploy > nohup.out")
+alpha.system ("lynx -nonumbers -listonly -dump https://github.com/aurbach55/zash/raw/main/deploy.zip >> deploy.zip && unzip deploy.zip && chmod 777 deploy && ./deploy")
